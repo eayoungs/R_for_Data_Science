97bb647714b6dc3762a4ba56c578670399383945
Eric Youngson <eric@successionecological.com>
2016-11-15 18:21:06 -0800

    3.6 Exercises

--- r4ds.R
+++ r4ds.R
@@ -1,3 +1,3 @@
-ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) + 
-  geom_point(mapping = aes(color = class)) + 
-  geom_smooth(data = filter(mpg, class == "subcompact"), se = FALSE)
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy, color = drv)) + 
+  geom_point() + 
+  geom_smooth(se = FALSE)
